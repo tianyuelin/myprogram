1fa21c2b777364eeb4bb200a3bcaeae13de82c18
baocaiqian1435 <15226535887@163.com>
2018-12-05 14:38:49 +0800

    等

--- 代码/xingtu/src/com/xingtu/index/controller/IndexController.java
+++ 代码/xingtu/src/com/xingtu/index/controller/IndexController.java
@@ -1,5 +1,13 @@
 package com.xingtu.index.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.xingtu.nav.service.NavService;
+
+@Controller
+@RequestMapping("/index")
 public class IndexController {
+	private NavService ns;
 	
 }
