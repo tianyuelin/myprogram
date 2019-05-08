b887ec6ac927c3ee6e9efb4aa6b66f3658a5233a
240436170 <240436170@qq.com>
2019-05-08 10:18:01 +0800

    q

--- 数据库/commentscore.sql
+++ 数据库/commentscore.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50532
 File Encoding         : 65001
 
-Date: 2019-05-08 08:43:14
+Date: 2019-05-08 10:17:17
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -21,11 +21,13 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `commentscore`;
 CREATE TABLE `commentscore` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(10) DEFAULT NULL,
   `name` varchar(20) DEFAULT NULL,
   `pingfen` varchar(10) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of commentscore
 -- ----------------------------
+INSERT INTO `commentscore` VALUES ('10', 'zhangi', '故宫', '6.0');
