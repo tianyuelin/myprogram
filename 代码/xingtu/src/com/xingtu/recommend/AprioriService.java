e331bd86755b8ae07b4a9148977b0554cefe2422
baocaiqian <2952635041@qq.com>
2019-05-28 11:06:53 +0800

    apriori

---
+++ 代码/xingtu/src/com/xingtu/recommend/AprioriService.java
@@ -0,0 +1,16 @@
+package com.xingtu.recommend;
+import java.util.List;
+import javax.annotation.Resource;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+@Service
+@Transactional(readOnly=false)
+public class AprioriService {
+	@Resource
+	private ReadData rd;
+	public List<List<String>> find(){
+		System.out.println("SERVICE");
+		return rd.findadd();
+	}
+
+}
---
+++ 代码/xingtu/src/com/xingtu/recommend/ReadData.java
@@ -0,0 +1,34 @@
+package com.xingtu.recommend;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ReadData {
+	@Resource
+	private SessionFactory sessionFactory;
+	public List<List<String>> findadd(){
+		List<List<String>> record = new ArrayList<List<String>>();
+		Session session=this.sessionFactory.getCurrentSession();
+		Query q=session.createQuery("select didian from Beijing");
+		List<String> list = q.list();
+		List<String> add = new ArrayList<String>();
+		for (String s : list) {
+			String[] a= s.split("：");
+			add.add(a[1]);
+		}
+		for(String s:add) {
+			String [] a = s.split(">");
+			record.add(Arrays.asList(a));
+		}
+		return record;
+	}
+}
